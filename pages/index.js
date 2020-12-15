2ed4763e20be81f0db486399ac6a36aa06771c6c
rolstenhouse <rolsthoorn12@gmail.com>
2020-12-14 22:09:33 -0800

    add basic server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,80 +1,59 @@
+import { execOnce } from "next/dist/next-server/lib/utils";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import MainPage from "./MainPage";
 
-export default function Home({ props }) {
+function Home({ userInfo, sub }) {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>{props.title}</title>
+        <title>{userInfo.title}</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description">{props.description}</meta>
+        <meta name="description" content={userInfo.description} />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XVSNB8VETM"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-XVSNB8VETM');
+              `,
+          }}
+        />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <MainPage userInfo={userInfo} sub={sub} />
+    </>
   );
 }
 
 export async function getServerSideProps(ctx) {
   const host = ctx.req.headers.host;
-  const sub = host.split(".")[0];
+  let sub = host.split(".")[0];
+  if (process.env.NODE_ENV === "development") {
+    sub = "parth";
+  }
 
   const res = await fetch(`https://api.withlaguna.com/stonks/userinfo/${sub}`);
   const userInfo = await res.json();
 
+  if (!userInfo) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
     props: {
       userInfo,
+      sub,
     },
   };
 }
+
+export default Home;
